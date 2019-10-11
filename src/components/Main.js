653c9c8bbd307ce41dcb2558a9bdb271aaaa3894
armanalz <armanalizadeh467@gmail.com>
2019-10-12 02:54:16 +0330

    adding images and styling the carousel

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,28 +5,57 @@ class Main extends Component {
 
     state = {
 
-        rotate: 0
+        rotate: 0,
+        i: 0,
+        circulate: 0,
+        description: {
+
+            price: [32,45,71,59,60,90,29,44],
+            title: ["green goddes chicken salad","ghorme","gheime","6andaz","ghoorzbij","kuiipala","khulabe","piazkhoroush"],
+            text: ["lorem ipsum dolor sit amet","lorem ipsum dolor sit amet","lorem ipsum dolor sit amet","lorem ipsum dolor sit amet","lorem ipsum dolor sit amet","lorem ipsum dolor sit amet","lorem ipsum dolor sit amet","lorem ipsum dolor sit amet"],
+
+        }
 
     }
 
     carouselHandler = (e) => {
      
-        if(e.target.id === "right"){
+        let  length = this.state.description.price.length-1;
+
+        this.setState({
+            circulate: this.state.circulate + 360
+        })
+
+        if(e.target.id === "right" && this.state.i < length) {
             this.setState({
-            rotate: this.state.rotate + 45
+                   rotate: this.state.rotate + 45, i: this.state.i+1
             })
-            console.log(e.target.id)
-        }else  if(e.target.id === "left"){
+
+        }else if(e.target.id === "right" && this.state.i >= length) {
 
             this.setState({
-                rotate: this.state.rotate - 45
-                })
-                console.log(e.target.id)
+                  rotate: this.state.rotate + 45, i: 0
+            })
+
+        }else if(e.target.id === "left" && this.state.i > 0) {
+
+            this.setState({
+                rotate: this.state.rotate - 45, i: this.state.i-1
+            })
+
+        }else if (e.target.id === "left" && this.state.i >= 0) {
+
+            this.setState({
+                rotate: this.state.rotate - 45, i: length
+            })
 
         }
 
     }
     render() {
+
+        let bg = require(`../resources/img/img-${this.state.i}.png`)
+
         return (
             <main className="main">
                 <header className="header">
@@ -59,10 +88,10 @@ class Main extends Component {
                 <div className="bottom-section">
 
                     <div className="description">
-                        <h1 className="description_price">$32</h1>
-                        <h2 className="description_title">green goddess chicken salad</h2>
+                        <h1 className="description_price">${this.state.description.price[this.state.i]}</h1>
+                        <h2 className="description_title">{this.state.description.title[this.state.i]}</h2>
                         <p className="description_text">lorem ipsum dolor sit amet loren ipsum dolor
-                                      sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+                                      sit amet lorem ipsum dolor sit amet {this.state.description.text[this.state.i]}
                          </p>
                         <div className="description_button">add to card</div>
                     </div>
@@ -77,7 +106,11 @@ class Main extends Component {
                             />
                         </div>
 
-                        <div className="controller_middle"></div>
+                        <div className="controller_middle" 
+                             style={{transform:`rotate(${this.state.circulate}deg)`,backgroundImage:`url(${bg})`}}
+                        >
+
+                        </div>
 
                         <div className="controller_arrow right" id="right"
                              onClick={(e) => this.carouselHandler(e)}
