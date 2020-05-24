75912409fcbf994b77554d36f242c9bc90718e8d
Arman Alizadeh <armanalizadeh467@gmail.com>
2020-05-25 00:31:58 +0200

    Updates

---
+++ precache-manifest.d2b6988f8237d2c22f09cc0136c1950b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de010d6e2ff1cdaf7fc9835aac227fa",
+    "url": "/foodpin/index.html"
+  },
+  {
+    "revision": "46518db38d28ebfe8a18",
+    "url": "/foodpin/static/css/main.48f31891.chunk.css"
+  },
+  {
+    "revision": "57e3a62120d84821c65b",
+    "url": "/foodpin/static/js/2.1bbaafd7.chunk.js"
+  },
+  {
+    "revision": "fb11ee5d4294d5cf1384c5f5357b7a8c",
+    "url": "/foodpin/static/js/2.1bbaafd7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46518db38d28ebfe8a18",
+    "url": "/foodpin/static/js/main.fdc8a80e.chunk.js"
+  },
+  {
+    "revision": "e209d0eff3dc6b2acddd",
+    "url": "/foodpin/static/js/runtime-main.71e1b211.js"
+  },
+  {
+    "revision": "ad5d802e797c3833d1792fe0ad44fe16",
+    "url": "/foodpin/static/media/img-0.ad5d802e.png"
+  },
+  {
+    "revision": "8a2b0947e20bc2ffebd8a9f686b59cd4",
+    "url": "/foodpin/static/media/img-1.8a2b0947.png"
+  },
+  {
+    "revision": "38e13b79e72cb900da5d4f81f0756f1b",
+    "url": "/foodpin/static/media/img-2.38e13b79.png"
+  },
+  {
+    "revision": "334336d00b2592b8f8ff2b1d3cdc57c4",
+    "url": "/foodpin/static/media/img-3.334336d0.png"
+  },
+  {
+    "revision": "555978c9301f3ce31a1378cf75dd21df",
+    "url": "/foodpin/static/media/img-4.555978c9.png"
+  },
+  {
+    "revision": "adf7c805befdeaf84bba762c0712363b",
+    "url": "/foodpin/static/media/img-5.adf7c805.png"
+  },
+  {
+    "revision": "41e99f2ce6838e2838dee072c3d43768",
+    "url": "/foodpin/static/media/img-6.41e99f2c.png"
+  },
+  {
+    "revision": "0c4c72a958ff06754edcfd94cab1a193",
+    "url": "/foodpin/static/media/img-7.0c4c72a9.png"
+  },
+  {
+    "revision": "fac7acb2afd5460eec73b0170ccc2178",
+    "url": "/foodpin/static/media/sprite.fac7acb2.svg"
+  }
+]);
